ba990f8bf1105f9d818c8ac7a2ce0491f391c067
munga114 <mungai.mbugua@student.moringaschool.com>
2022-04-25 02:16:04 +0300

    add importations

---
+++ run.py
@@ -0,0 +1,8 @@
+from User import User
+from threading import Thread
+from getpass import getpass
+from pyfiglet import Figlet
+from terminaltables import AsciiTable
+import sys
+
+f = Figlet(font='mini')
