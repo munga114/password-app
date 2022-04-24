8f60acc297403fe518a78694c49560f2a523bc1e
munga114 <mungai.mbugua@student.moringaschool.com>
2022-04-25 01:41:56 +0300

    create constructor for class user

--- User.py
+++ User.py
@@ -3,3 +3,9 @@ import pyperclip as cb
 
 
 class User(Credentials):
+    
+    def __init__(self):
+        super().__init__()
+        self.username = None
+        self.password = None
+        self.token = None
